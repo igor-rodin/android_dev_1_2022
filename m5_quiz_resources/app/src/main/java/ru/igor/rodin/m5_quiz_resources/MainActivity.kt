a9aaee587fa011de7e24499e7ca0272cf0956fff
Igor Rodin <igreen1974@gmail.com>
2024-02-18 20:56:10 +0300

    Update MainActivity

--- m5_quiz_resources/app/src/main/java/ru/igor/rodin/m5_quiz_resources/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/ru/igor/rodin/m5_quiz_resources/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.igor.rodin.m5_quiz_resources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.igor.rodin.m5_quiz_resources.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 }
