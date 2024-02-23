bf7d74c43654558743a3b7bcbcd41fcef3973284
Igor Rodin <igreen1974@gmail.com>
2024-02-23 15:18:22 +0300

    Add quiz model

---
+++ m7_quiz_fragments/app/src/main/java/ru/igor/rodin/m7_quiz_fragments/quiz/Question.kt
@@ -0,0 +1,3 @@
+package ru.igor.rodin.m7_quiz_fragments.quiz
+
+class Question(val question: String, val answers: List<String>, val feedback: List<String>)
---
+++ m7_quiz_fragments/app/src/main/java/ru/igor/rodin/m7_quiz_fragments/quiz/Quiz.kt
@@ -0,0 +1,5 @@
+package ru.igor.rodin.m7_quiz_fragments.quiz
+
+interface Quiz {
+    val questions: List<Question>
+}
