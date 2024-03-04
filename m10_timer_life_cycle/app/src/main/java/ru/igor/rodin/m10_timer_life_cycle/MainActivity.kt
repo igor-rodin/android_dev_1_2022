6f71e8967876eab903f0a09374387247df5a58fd
Igor Rodin <igreen1974@gmail.com>
2024-03-05 01:53:04 +0300

    Init homework 10

---
+++ m10_timer_life_cycle/app/src/main/java/ru/igor/rodin/m10_timer_life_cycle/MainActivity.kt
@@ -0,0 +1,39 @@
+package ru.igor.rodin.m10_timer_life_cycle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.igor.rodin.m10_timer_life_cycle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        _binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initUI()
+    }
+
+    private fun initUI() {
+        with(binding) {
+            timerProgress.max = 60
+            timerProgress.progress = 30
+            cdtValue.text = timerProgress.progress.toString()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
