8ea252cb1b565eb593ffe38efc9b304a0d53208f
Igor Rodin <igreen1974@gmail.com>
2024-03-20 15:17:04 +0300

    Add retrofit configuration

---
+++ m14_retrofit/app/src/main/java/ru/igor/rodin/retrofit/RandomUserApp.kt
@@ -0,0 +1,35 @@
+package ru.igor.rodin.retrofit
+
+import android.app.Application
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ru.igor.rodin.retrofit.data.RandomUserApi
+
+private const val BASE_URL = "https://randomuser.me/api"
+
+class RandomUserApp : Application() {
+
+    lateinit var randomUserApi: RandomUserApi
+
+    override fun onCreate() {
+        super.onCreate()
+        initRetrofit()
+    }
+
+    private fun initRetrofit() {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        randomUserApi = retrofit.create(RandomUserApi::class.java)
+    }
+}
